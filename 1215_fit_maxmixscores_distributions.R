92c3aff824345ae5265464d44e49acf146d91bd6
breezedu <breeze.du@gmail.com>
2016-12-15 19:29:06 -0500

    update check maxium mixscore ADM percentile R

--- 1215_fit_maxmixscores_distributions.R
+++ 1215_fit_maxmixscores_distributions.R
@@ -18,22 +18,109 @@
 ## the folder where I put all the mixscore max vectors
 routine <- 'D:/GitHubRepositories/ADM_Statistic_Data/CircleShuffle/Out_results/'
 
-## there are 12 max mixscore ADM vectors, each contains 1001 max values of mixscores, the first one is the unshuffled dataset's max value
-max.vectors <- rep(NA, 12)
+## there are 13 max mixscore ADM vectors, each contains 1001 max values of mixscores, the first one is the unshuffled dataset's max value
+max.vectors <- rep(NA, 13)
 
+
+### get the max ADM mixscore value from unshuffled dataset
+### the first colum represent result from the unshuffled dataset
+test.vectors <- read.table("D:/GitHubRepositories/ADM_Statistic_Data/CircleShuffle/Out_results/max_mixscore_1.txt", header = F)
+test.vector <- test.vectors$V1
+
+# get the first max value
+max.unshuffled <- test.vector[1]
+
+# print check
+max.unshuffled
+
+
+## initial an empty vector, to store all max values from 10,000 datasets of shuffled datasets.
+vector.all <- NULL
+
+
+#######################################################
+## use a for loop, to get all datasets,
+## read in each datasets as a table, 
+## get the first colum 'V1', which is a vector
+## combine the current vector with vector.all
+## check the length and summary of the vector.all
+#######################################################
+for( i in 1:10){
+  
+  ##// use paste() method to get routines for all 10 files
+  file.name <- paste(routine, 'max_mixscore_', i, '.txt', sep = '')
+  
+  ## check the filr name
+  print(file.name)
+  
+  ## read in table, then pick the first colum V1
+  max.vectors <- read.table(file.name, header = F )
+  
+  print( summary(max.vectors) )
+  print( sum(max.vectors > max.unshuffled))
+  
+  vector.all <- c(vector.all, max.vectors$V1)
+}
+
+## check the length of the max.values.vector
+length(vector.all)
+
+## summary
+summary(vector.all)
+
+## 
+sum(vector.all > max.unshuffled)
+
+## over all 10,000 shuffled datasets, there are 179 max values greater than the max mixscore value of unshuffled dataset
+
+## check the percentile of maxium mixscore ADM results greater than that of the unshuffled dataset 
+sum(vector.all > max.unshuffled) / (length(vector.all) - 10)
+
+
+####################
+##  [1] 0.0179    ##
+####################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## extract vectors from tables
 for( i in 1:12){
   
-  file.name <- paste(routine, 'max_mixscore_', i, '.txt', sep = "")
+  max.vectors[i] <- max.vectors[i]$V1
   
-  print(file)
+  print( head(max.vectors[i]) )
   
-  
-  max.vectors[i] <- scan(file.name, what = character() )
-  
-  print( summary(max.vectors[i]) )
 }
 
 
+max.vectors[4]
+
+vector.all <- NULL
+for(i in 1:12){
+  
+  print(summary(max.vectors[i]))
+  
+}
+
+length(vector.all)
+
+
+summary(vector.all)
+
+length(max.vectors[2])
 file.name <- paste(routine, 'max_mixscore_', 2, '.txt', sep = "")
 
 table.2 <- read.table(file.name, header = F)
